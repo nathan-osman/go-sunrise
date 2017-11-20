ea7ae059829a655af1b1d5e9165cfeab7bf5268d
Nathan Osman <nathan@quickmediasolutions.com>
2017-11-20 11:56:53 -0800

    Update documentation for SolarTransit.

--- transit.go
+++ transit.go
@@ -2,6 +2,8 @@ package sunrise
 
 import "math"
 
+// SolarTransit calculates the Julian data for the local true solar transit.
 func SolarTransit(solarNoon, solarAnomaly, eclipticLongitude float64) float64 {
-	return 2451545.5 + solarNoon + 0.005*math.Sin(solarAnomaly) - 0.0069*math.Sin(2*eclipticLongitude)
+	return 2451545.5 + solarNoon + 0.005*math.Sin(solarAnomaly) -
+		0.0069*math.Sin(2*eclipticLongitude)
 }
--- transit_test.go
+++ transit_test.go
@@ -2,24 +2,26 @@ package sunrise
 
 import "testing"
 
-var dataSolarTransite = []struct {
+var dataSolarTransit = []struct {
 	inSolarNoon         float64
 	inSolarAnomaly      float64
 	inEclipticLongitude float64
 	out                 float64
 }{
-	// April 1, 2004. 5 degrees east longitude. This is a test problem from (http://aa.quae.nl/en/reken/zonpositie.html)
-	// The problem was slightly modified to take Julian leap seconds into account in anomaly.go. Expect small deviations from the above site.
+	// April 1, 2004 - 5 degrees east longitude; this is a test problem from
+	// (http://aa.quae.nl/en/reken/zonpositie.html) - the problem was slightly
+	// modified to take Julian leap seconds into account in anomaly.go; expect
+	// small deviations from the above site
 	{1552.01468889, 87.19521, 12.05293, 2453096.9393},
 	// Prime meridian on January 1, 1990
 	{2447893, 281.55531, 202.56998, 4899438.48992},
-	// Toronto. January 1, 2000
+	// Toronto - January 1, 2000
 	{2451545.2205, 201.18486, 202.22912, 4903090.71375},
 }
 
 func TestSolarTransit(t *testing.T) {
-	for _, tt := range dataSolarMeanAnomaly {
-		v := SolarMeanAnomaly(tt.in)
+	for _, tt := range dataSolarTransit {
+		v := SolarTransit(tt.inSolarNoon, tt.inSolarAnomaly, tt.inEclipticLongitude)
 		if Round(v, DefaultPlaces) != Round(tt.out, DefaultPlaces) {
 			t.Fatalf("%f != %f", v, tt.out)
 		}
