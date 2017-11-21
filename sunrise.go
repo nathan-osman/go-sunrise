92ce2d8333ba602374b733609ca7661e2b6e9aed
Nathan Osman <nathan@quickmediasolutions.com>
2017-11-20 23:16:17 -0800

    Remove calculation of declination and add SunriseSunset function.

---
+++ sunrise.go
@@ -0,0 +1,22 @@
+package sunrise
+
+import (
+	"time"
+)
+
+// SunriseSunset calculates when the sun will rise and when it will set on the
+// given day at the specified location.
+func SunriseSunset(latitude, longitude float64, year int, month time.Month, day int) (time.Time, time.Time) {
+	var (
+		d                 = MeanSolarNoon(longitude, year, month, day)
+		solarAnomaly      = SolarMeanAnomaly(d)
+		equationOfCenter  = EquationOfCenter(solarAnomaly)
+		eclipticLongitude = EclipticLongitude(solarAnomaly, equationOfCenter, d)
+		solarTransit      = SolarTransit(d, solarAnomaly, eclipticLongitude)
+		hourAngle         = HourAngle(latitude, eclipticLongitude)
+		frac              = hourAngle / 360
+		sunrise           = solarTransit + frac
+		sunset            = solarTransit - frac
+	)
+	return JulianDayToTime(sunrise), JulianDayToTime(sunset)
+}
